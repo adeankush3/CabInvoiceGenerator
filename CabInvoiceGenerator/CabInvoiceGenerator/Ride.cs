13f8315d8cc1385e522342dcbbed1ae5828395e3
adeankush3 <adeankush3@gmail.com>
2022-03-13 17:31:29 +0530

    [Ankush]Multiple ride

--- CabInvoiceGenerator/CabInvoiceGenerator/Ride.cs
+++ CabInvoiceGenerator/CabInvoiceGenerator/Ride.cs
@@ -6,18 +6,16 @@ using System.Threading.Tasks;
 
 namespace CabInvoiceGenerator
 {
-    internal class Ride
+    public class Ride
     {
+        // variables used
         public double distance;
         public int time;
-        private double v1;
-        private int v2;
 
         public Ride(double distance, int time)
         {
             this.distance = distance;
             this.time = time;
         }
-
     }
 }
